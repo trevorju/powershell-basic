b212696648a402ae5a390d32cc07af5846089d2a
trevorju <trevor.juurma@voco.ee>
2023-11-23 13:40:54 +0200

    pr7 - solution

---
+++ pr7.ps1
@@ -0,0 +1,12 @@
+﻿$groups = @("Red","Green","Yellow","Blue")
+$result = @()
+for($i = 1 ; $i-le 20 ; $i++) 
+{ 
+$g = Get-Random $groups 
+$temp = [PSCustomObject]@{
+ RollNumber = $i
+  Group = $g 
+  } 
+  $result += $temp 
+  }
+$result
