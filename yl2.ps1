02b3b77f28fba7b8416acdc60e25f70dff3e669d
trevorju <trevor.juurma@voco.ee>
2023-11-23 13:53:50 +0200

    yl2 - kasutaja kustutamine sisestatud ees- ja perekonnanime järgi

---
+++ yl2.ps1
@@ -0,0 +1,21 @@
+﻿Write-Host "Palun sisestage vajalikud andmed konto kustutamiseks!"
+$ees = read-host "Sisesta eesnimi: " 
+$pere = read-host "Sisesta perekonnanimi: "
+
+$kas = $ees.ToLower() 
+$su = $pere.Tolower()
+$kasutaja = "$kas.$su"
+Write-Host "Kustutav konto on $kasutaja"
+
+
+$olemasolev = Get-LocalUser -Name "$kasutaja" -ErrorAction SilentlyContinue
+
+if ($olemasolev) {
+    Remove-LocalUser -Name $kasutaja -Verbose
+    Write-Host "Kasutaja $kasutaja on kustutatud"
+}
+else {
+    Write-Host "Kasutaja kustutamisel tekkis viga"
+
+}
+
