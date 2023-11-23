a5365aeffd513016dd0f58c7fb318a2425712eb8
trevorju <trevor.juurma@voco.ee>
2023-11-23 13:39:07 +0200

    yl1 - konto tegemine eelnevalt valitud parooliga

---
+++ yl1.ps1
@@ -0,0 +1,33 @@
+﻿
+
+  
+$ees = read-host "Sisesta oma eesnimi: "
+
+$pere = read-host "Sisesta oma perekonnanimi: "
+
+$kas = $ees.ToLower() 
+$su = $pere.Tolower()
+$kasutaja = "$kas.$su"
+Write-Host "Loodav kasutaja on $kasutaja"
+
+
+$olemasolev = Get-LocalUser -Name "$kasutaja" -ErrorAction SilentlyContinue
+
+$KasutajaParool = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
+
+if (-not $olemasolev) {
+   New-LocalUser "$kasutaja" -Password $KasutajaParool -FullName "$ees $pere" -Description "local account - $ees $pere"
+     Write-Host "Kasutaja: $kasutaja"
+     Write-Host "Nimi: $ees $pere"
+    }
+else {
+     
+      Write-Host "Kasutaja loomisega on tekkinud probleem"
+    exit
+}
+
+
+sleep 10
+
+
+Get-LocalUser
