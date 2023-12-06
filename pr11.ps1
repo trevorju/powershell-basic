42df0aaa72a1aa93dc1172b5cc65c00bf0bf6f63
trevorju <trevor.juurma@voco.ee>
2023-12-06 22:39:30 +0200

    Prac 11 solution

---
+++ pr11.ps1
@@ -0,0 +1,36 @@
+﻿$n1 = Read-Host "Sisesta esimene number"
+$n2 = Read-Host "Sisesta teine number"
+
+Write-Host "Kalkulaator" -ForegroundColor Green
+Write-Host "1: +" -ForegroundColor Yellow
+Write-Host "2: -" -ForegroundColor Yellow
+Write-Host "3: /" -ForegroundColor Yellow
+Write-Host "4: *" -ForegroundColor Yellow
+
+$ch = Read-Host "Sisesta oma valik"
+
+switch ($ch) {
+    1 {
+        $s = $n1 + $n2
+        Write-Host "Summa on: $s"
+    }
+    2 {
+        $s = $n1 - $n2
+        Write-Host "Lahus on: $s"
+    }
+    3 {
+        if ($n2 -eq 0) {
+            Write-Host "Nulliga ei saa jagada" -ForegroundColor Red
+        } else {
+            $s = $n1 / $n2
+            Write-Host "Jagatis on: $s"
+        }
+    }
+    4 {
+        $s = $n1 * $n2
+        Write-Host "Korrutis on: $s"
+    }
+    default {
+        Write-Host "Valisite vale valiku." -ForegroundColor Red
+    }
+}
