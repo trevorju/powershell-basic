02ea05d889a7856f263e551e2f60ffbba5d448ae
trevorju <trevor.juurma@voco.ee>
2023-12-06 23:01:57 +0200

    Prac 12 solution

---
+++ pr12.ps1
@@ -0,0 +1,41 @@
+﻿function runstart 
+{
+$start = Get-Service | ?{$_.Status -eq "Running"}
+Write-Host "Töötavaid programme on" $start.count
+}
+
+runstart
+
+function stop
+
+{
+$stop = Get-Service | ?{$_.Status -eq "Stopped"}
+Write-host "Peatatud programmide arv on" $stop.count
+}
+
+stop
+
+#YL 2
+sleep 4
+
+
+function autoread
+{
+     param
+        (
+         [string]$status
+        )
+        if ($status -eq "Running")
+         {
+             $start = Get-Service | ?{$_.Status -eq "Running"}
+             Write-Host "Töötavaid programme on" $start.count
+         }
+        elseif ($status -eq "Stopped")        
+         {
+             $stop = Get-Service | ?{$_.Status -eq "Stopped"}
+             Write-host "Peatatud programmide arv on" $stop.count
+         }
+}
+
+autoread -status Running
+autoread -status Stopped
