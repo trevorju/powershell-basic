940833ca117d8646743d08a76db45e4ba66d5618
trevorju <trevor.juurma@voco.ee>
2023-12-06 21:52:01 +0200

    Pract 8 solution

---
+++ 8pr.ps1
@@ -0,0 +1,8 @@
+﻿
+while ((Get-Process -Name "Notepad" -ErrorAction SilentlyContinue).Count -gt 0) {
+    Write-Host "Notepad is running"
+    Start-Sleep -Seconds 2  
+}
+
+Write-Host "All instances of Notepad are closed"
+
