5553910f5d0bb1a06437f7ee77207f5187b6a2f5
trevorju <trevor.juurma@voco.ee>
2023-12-06 22:38:59 +0200

    Prac 10 solution

---
+++ pr10.ps1
@@ -0,0 +1,17 @@
+﻿$ct1 = 0
+$ct2 = 0
+
+do {
+    $ch = Get-Process | Where-Object { $_.Name -like "note*" }
+
+    if ($ch.Count -gt 0) {
+        Write-Host "Notepad is running"
+        Start-Sleep -Seconds 1
+        $ct1++
+        $ct2 = 2
+    } else {
+        $ct2 = 1
+    }
+} until ($ct2 -eq 1)
+
+$ct1
