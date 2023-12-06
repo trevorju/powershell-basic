888184e089ec5053adf2ce78ead69d112a06f24d
trevorju <trevor.juurma@voco.ee>
2023-12-06 22:06:16 +0200

    Prac 9 solution

---
+++ pr9.2.ps1
@@ -0,0 +1,15 @@
+﻿$ct1 = 0
+
+do {
+    $ch = Get-Process | Where-Object { $_.Name -like "notepad" }
+
+    if ($ch.Count -gt 0) {
+        Write-Host "Notepad is running"
+        $ct1++
+    }
+
+    Start-Sleep -Seconds 1
+} while ($ch.Count -gt 0)
+
+
+$ct1 
---
+++ pr9.ps1
@@ -0,0 +1,15 @@
+﻿$ct1 = 0
+do
+    {
+        $ch = $null
+        $ch = Get-Process | ?{$_.name -like "note*"}
+    if($ch -ne $null)
+        {$ct2 = 2
+        Write-Host "Notepad is running"
+        Start-Sleep -Seconds 1
+        $ct1++
+    }
+    else
+    {$ct2 = 1}
+}while($ct2 -ne 1)
+$ct1
